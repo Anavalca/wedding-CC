12070b35158259ebf3ff3c6eed29655832e3fbb6
anavalca <avc92j@gmail.com>
2023-06-11 14:06:55 +0200

    update footer styles at mobile and dvh

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,8 @@ export const Footer = () => {
   return (
     <footer >
       <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">
-        <img className="w-16" alt="Licencia de Creative Commons" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"/>
-        <span className="ml-2">Este obra está bajo una licencia de Creative Commons.</span>
+        <img alt="Licencia de Creative Commons" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png"/>
+        <span>Este obra está bajo una licencia de Creative Commons.</span>
       </a>
       <a href="https://ana-valdivia.netlify.app/#/" alt='Ana Valdivia Portfolio' title='Ana Valdivia Portfolio' target='_blank' rel='noreferrer noopener'><span>Web realizada y diseñada por <span className="name">Ana Valdivia Cano</span>.</span></a>
     </footer >
